afbb052df8f26930b3e962e97aadd4a1cfab9da3
Tobyha01 <tobyhamilton01@gmail.com>
2023-02-20 04:00:10 +0000

    Created a new file for estabilishing new db connection

---
+++ db.js
@@ -0,0 +1,12 @@
+const {Sequelize, DataTypes} = require("sequelize")
+const [path] = require("path")
+
+const sequelize = new Sequelize({
+    dialect: "sqlite",
+    storage: path.join(__dirname, "db.sqlite")
+})
+
+module.exports = {
+    DataTypes,
+    sequelize
+}
