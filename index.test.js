d41f0d2f8f267da1e200c4f480cc1462c692df29
Tobyha01 <tobyhamilton01@gmail.com>
2023-02-20 07:45:31 +0000

    Added a test to check a user could be created

--- index.test.js
+++ index.test.js
@@ -1,3 +1,15 @@
 const {sequelize, DataTypes} = require("./db")
 const {User, Board, Cheese} = require("./models/index")
 
+describe("User, Board and Cheese test suite", function() {
+    beforeAll(async function() {
+        await sequelize.sync({force: true})
+    })
+
+    test("Can create a user", async function() {
+        const user = await User.create({name: "Steve", email: "steve123@hotmail.com"})
+        expect(user.name).toBe("Steve")
+        expect(user.email).toBe("steve123@hotmail.com")
+    })
+    
+})
