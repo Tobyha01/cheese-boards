ee9d43192621b0c3b7de98604b1ef1b30aa0793b
Tobyha01 <tobyhamilton01@gmail.com>
2023-02-20 18:00:22 +0000

    canged cheese reference where defining the Cheese db

--- models/Cheese.js
+++ models/Cheese.js
@@ -1,6 +1,6 @@
 const {sequelize, DataTypes} = require("../db")
 
-const Cheese = sequelize.define("Cheese", {
+const Cheese = sequelize.define("cheese", {
     title: DataTypes.STRING,
     description: DataTypes.STRING
 })
